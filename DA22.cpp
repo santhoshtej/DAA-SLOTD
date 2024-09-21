ce3493f6ed492c8122850dc2132e7b8fcd395223
santhoshtej <141199693+santhoshtej@users.noreply.github.com>
2024-09-21 13:00:52 +0530

    Add files via upload

---
+++ DA22.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int linear_search(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i; 
+        }
+    }
+    return -1; 
+}
+
+int main() {
+    int size, target;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the value to search for: ");
+    scanf("%d", &target);
+
+    int result = linear_search(arr, size, target);
+
+    if (result != -1) {
+        printf("Element %d found at index %d.\n", target, result);
+    } else {
+        printf("Element %d not found in the array.\n", target);
+    }
+
+    return 0;
+}
